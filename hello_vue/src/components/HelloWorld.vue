093db0b345b5299cb3300d8222857c12784310fe
mm-masahiro <hand5.mm9.mzh@gmail.com>
2020-12-22 00:42:45 +0900

    v-~~ practice

--- hello_vue/src/components/HelloWorld.vue
+++ hello_vue/src/components/HelloWorld.vue
@@ -1,86 +1,10 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <p>{{ text }}</p>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <h2 v-show="showText">{{ text }}</h2>
+    <!-- v-onでイベント発火時の JavaScript の実行が可能に -->
+    <!-- ↓は、toggle関数の呼び出し -->
+    <button v-on:click="toggle">toggle</button>
   </div>
 </template>
 
@@ -89,10 +13,17 @@ export default {
   name: 'HelloWorld',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
-      text: "hogehoge"
+      msg: 'Hello Vue.js App',
+      text: "hogehoge",
+      showText: true
     };
   },
+  methods: {
+    toggle: function() {
+      // data内で定義した値はthis.~~~のようにthisを使って参照していく
+      this.showText = !this.showText
+    }
+  }
 };
 </script>
 
